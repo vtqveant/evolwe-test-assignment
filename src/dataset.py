8cebad05e25bcd097e7552b8846c7e4f6a77d8ce
Konstantin Sokolov <vtqveant@gmail.com>
2022-09-04 15:02:24 +0300

    final results

--- src/dataset.py
+++ src/dataset.py
@@ -45,32 +45,6 @@ def main():
             break
         print(entry)
 
-    random_seed = 42
-    test_split_portion = 0.1
-
-    n_samples = len(dataset)
-    indices = list(range(n_samples))
-    split_idx = int(np.floor(test_split_portion * n_samples))
-
-    # shuffle
-    np.random.seed(random_seed)
-    np.random.shuffle(indices)
-
-    train_indices, test_indices = indices[split_idx:], indices[:split_idx]
-
-    # samplers
-    train_sampler = SubsetRandomSampler(train_indices)
-    test_sampler = SubsetRandomSampler(test_indices)
-
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=10, sampler=train_sampler)
-    test_loader = torch.utils.data.DataLoader(dataset, batch_size=10, sampler=test_sampler)
-
-    for sample in train_loader:
-        print(sample)
-
-    print(len(train_loader))
-    print(len(test_loader))
-
 
 if __name__ == '__main__':
     main()
